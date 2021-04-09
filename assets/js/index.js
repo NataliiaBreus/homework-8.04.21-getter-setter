967db5ae56abccbbc96dfa8e7a53372a1fda64ea
NataliiaBreus <breusnataly@gmail.com>
2021-04-09 08:15:03 +0300

    validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,16 @@
 class RangeValidator (from, to) {
   this.from = from;
   this.to = to;
+  if (typeof from !== "number" || typeof to !== "number"){
+    throw new TypeError ("This symbol is not a number!");
+  }
+function validate (number) {
+  if number >= this.from && number <= this.to{
+    return number;
+  }
+  throw new TypeError ("Wrong number!");
+}
+
   set from (newFrom) {
     this._from = newFrom;
   };
@@ -16,9 +26,10 @@ class RangeValidator (from, to) {
     return this._to;
   }
 };
-getRange(){
+get Range(){
 return `${this.from} ${this.to}`;
 }
+
 
   
 
